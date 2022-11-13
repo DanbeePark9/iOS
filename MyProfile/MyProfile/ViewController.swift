a4381c9157bc9ead3521f96114732edfb4b63634
Danbi Park <64319909+DanbeePark9@users.noreply.github.com>
2022-11-13 14:43:14 +0900

    MyProfile
    MyProfile

---
+++ MyProfile/MyProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyProfile
+//
+//  Created by 박단비 on 2022/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
