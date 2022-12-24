8bb361332fca6ef94616634e1052801fbb78a485
Danbee Park <64319909+DanbeePark9@users.noreply.github.com>
2022-12-24 20:01:20 +0900

    Add files via upload

---
+++ DesignCodeiOS15/DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by 박단비 on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            MatchedView()
+        }
+    }
+}
